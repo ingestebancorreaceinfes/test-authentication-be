2052ba2f61a6d0b2fd25e0f19ab187d62e247fc4
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2023-03-24 12:04:00 -0500

    Cambiando método de conexión de PostgreSQLv2

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,19 +34,11 @@ import { ConfigModule } from '@nestjs/config';
     // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL
-      // host: process.env.HOST,
-      // port: parseInt(process.env.PORT),
-      // username: process.env.USERNAME,
-      // password: process.env.PASSWORD,
-      // database: process.env.DATABASE,
-      // entities: [ User, Question ],
-      // subscribers: [ QuestionSubscriber ],
-      // autoLoadEntities: true,
-      // synchronize: true,
-      // extra: { 
-      //   "ssl": process.env.SSL 
-      // },
+      url: process.env.DATABASE_URL,
+      entities: [ User, Question ],
+      subscribers: [ QuestionSubscriber ],
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     // WinstonModule.forRoot({
     //   transports: [
